561f90611e6f602e6288d51b17de99f23eeb297e
alai13 <alan_lai@jhu.edu>
2019-12-28 23:08:00 +0800

    Showed number of neighbouring mines

--- Minesweeper/minesweeper_cell_v1.py
+++ Minesweeper/minesweeper_cell_v1.py
@@ -26,28 +26,49 @@ class Cell(QWidget):
         # Set up the parameters
         self.is_revealed = False
         self.is_flagged = False
-        self.is_bomb = False
+        self.is_mine = False
         self.initially_clicked = False
+        self.num_adjacent = 0
+
+    def reset_cell(self):
+        self.is_revealed = False
+        self.is_flagged = False
+        self.is_mine = False
+        self.initially_clicked = False
+        self.num_adjacent = 0
+        self.update()
 
     def paintEvent(self, event):
         p = QPainter(self)
         p.setRenderHint(QPainter.Antialiasing)
+
         r = event.rect()
-        outer, inner = Qt.darkBlue, Qt.cyan
-        p.fillRect(r, QBrush(inner))
-        pen = QPen(outer)
-        pen.setWidth(1)
-        p.setPen(pen)
-        p.drawRect(r)
 
         if self.is_revealed:
-            if self.is_bomb:
+            if self.is_mine:
                 p.drawPixmap(r, QPixmap(IMAGE_MINE))
-            else:
+            elif self.num_adjacent == 0:
                 p.drawPixmap(r, QPixmap(IMAGE_GRAY))
+            else:
+                pen = QPen(Qt.black)
+                p.setPen(pen)
+                f = p.font()
+                f.setBold(True)
+                p.setFont(f)
+                p.drawText(r, Qt.AlignHCenter | Qt.AlignVCenter, str(self.num_adjacent))
+                p.fillRect(r, QBrush(QColor(128, 128, 255, 128)))
 
         elif self.is_flagged:
             p.drawPixmap(r, QPixmap(IMAGE_FLAG))
+
+        else:
+            outer, inner = Qt.darkBlue, Qt.cyan
+            p.fillRect(r, QBrush(inner))
+            pen = QPen(outer)
+            pen.setWidth(1)
+            p.setPen(pen)
+            p.drawRect(r)
+
 
     def reveal(self):
         self.is_revealed = True
@@ -70,8 +91,8 @@ class Cell(QWidget):
 
         # Checks that the mouse is released over the same cell it was pressed on
         # Note (0, 0) is the top left corner of the cell, so only needs to be within button size of origin
-        if 0 <= e.x() <= self.button_size:
-            if 0 <= e.y() <= self.button_size:
+        if -5 <= e.x() <= self.button_size + 5:
+            if -5 <= e.y() <= self.button_size + 5:
 
                 # If it was the right click and not revealed, set the flag
                 if e.button() == Qt.RightButton and not self.is_revealed:
--- Minesweeper/minesweeper_v1.py
+++ Minesweeper/minesweeper_v1.py
@@ -1,5 +1,6 @@
 import math
 import sys
+import random
 
 import numpy as np
 from PyQt5.QtGui import *
@@ -17,6 +18,7 @@ class MainWindow(QMainWindow):
         self.title = 'Alan\'s Minesweeper'
         self.board_x_size = 20
         self.board_y_size = 10
+        self.num_mines = 30
         self.setWindowTitle(self.title)
 
         w = QWidget()
@@ -36,6 +38,7 @@ class MainWindow(QMainWindow):
 
         self.setCentralWidget(w)
         self.init_map()
+        self.set_up_map()
         self.show()
 
     def init_map(self):
@@ -44,6 +47,39 @@ class MainWindow(QMainWindow):
             for y in range(0, self.board_y_size):
                 w = Cell(x, y)
                 self.grid.addWidget(w, y, x)
+
+    def set_up_map(self):
+
+        positions = []
+        while len(positions) < self.num_mines:
+            x = random.randint(0, self.board_x_size - 1)
+            y = random.randint(0, self.board_y_size - 1)
+            if (x, y) not in positions:
+                w = self.grid.itemAtPosition(y, x).widget()
+                w.is_mine = True
+                positions.append((x, y))
+
+        # Add adjacencies to the positions
+        for x in range(0, self.board_x_size):
+            for y in range(0, self.board_y_size):
+                w = self.grid.itemAtPosition(y, x).widget()
+                w.num_adjacent = self.get_adjacency_n(x, y)
+                print(w.num_adjacent)
+
+    def get_adjacency_n(self, x, y):
+        positions = self.get_surrounding(x, y)
+        n_mines = sum(1 if w.is_mine else 0 for w in positions)
+
+        return n_mines
+
+    def get_surrounding(self, x, y):
+        positions = []
+
+        for xi in range(max(0, x - 1), min(x + 2, self.board_x_size)):
+            for yi in range(max(0, y - 1), min(y + 2, self.board_y_size)):
+                positions.append(self.grid.itemAtPosition(yi, xi).widget())
+
+        return positions
 
 
 if __name__ == '__main__':
