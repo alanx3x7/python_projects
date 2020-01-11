2926eea89e62b17717fde6f264cd692fdef6f9b6
alai13 <alan_lai@jhu.edu>
2020-01-11 15:24:30 +0800

    Changed piece coordinate organization

--- Tetris/tetris_board.py
+++ Tetris/tetris_board.py
@@ -4,6 +4,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 
 import numpy as np
+import time
 
 from tetris_tetromino import Tetromino
 
@@ -19,6 +20,7 @@ class TetrisBoard(QWidget):
         self.speed = 300
         self.cell_side_length = 30
         self.start_pos = (4, 0)
+        self.colour_table = [0x000000, 0xFF0000, 0x00FF00, 0x00FFFF, 0x800080, 0xFFFF00, 0xFFA500, 0x0000FF]
 
         self.x_cell_corners = np.zeros(10)
         for x in range(self.width):
@@ -33,6 +35,7 @@ class TetrisBoard(QWidget):
         self.timer = QBasicTimer()
 
         self.floating = Tetromino(self.start_pos)
+        self.setFocusPolicy(Qt.StrongFocus)
 
         QApplication.processEvents()
 
@@ -56,12 +59,16 @@ class TetrisBoard(QWidget):
 
         for y in range(self.height):
             for x in range(self.width):
+                if self.board[y, x] == 0:
+                    p.setBrush(QBrush(inner, Qt.SolidPattern))
+                else:
+                    p.setBrush(QBrush(QColor(self.colour_table[int(self.board[y, x])]), Qt.SolidPattern))
                 p.drawRect(self.x_cell_corners[x], self.y_cell_corners[y], 30, 30)
 
     def paintFloat(self, p):
 
         outer = Qt.white
-        inner = Qt.green
+        inner = QColor(self.colour_table[self.floating.identity.value])
 
         p.setPen(QPen(outer, 2, Qt.SolidLine))
         p.setBrush(QBrush(inner, Qt.SolidPattern))
@@ -95,9 +102,10 @@ class TetrisBoard(QWidget):
             self.update()
         else:
             self.fix_floating_piece()
-            self.floating.update_center(self.start_pos[0], self.start_pos[1])
-            self.floating.random_assign_shape()
-            self.update()
+            if self.create_new_piece():
+                self.update()
+            else:
+                print("Gameover")
 
     def move_floating_piece(self, x, y):
 
@@ -115,9 +123,42 @@ class TetrisBoard(QWidget):
             if new_x >= self.width or new_x < 0 or new_y >= self.height:
                 return False
 
+            if self.board[new_y, new_x] != 0:
+                return False
+
         return True
 
     def fix_floating_piece(self):
 
         for positions in self.floating.coordinates:
-            self.board[positions[1], positions[0]] = self.floating.identity.value
+            if 0 <= positions[0] < self.width and 0 <= positions[1] < self.height:
+                self.board[positions[1], positions[0]] = self.floating.identity.value
+
+    def create_new_piece(self):
+        self.floating.update_center(self.start_pos[0], self.start_pos[1])
+        self.floating.random_assign_shape()
+        for positions in self.floating.coordinates:
+            if positions[0] > -1 and positions[1] > -1:
+                if self.board[positions[1], positions[0]] != 0:
+                    return False
+        return True
+
+    def rotate_floating_piece(self, direction):
+        if direction == 1:
+            self.floating.rotate_left()
+            self.update()
+
+    def keyPressEvent(self, event):
+
+        key = event.key()
+
+        if key == Qt.Key_Left:
+            self.move_floating_piece(-1, 0)
+        elif key == Qt.Key_Right:
+            self.move_floating_piece(1, 0)
+        elif key == Qt.Key_Down:
+            self.move_floating_piece(0, 1)
+        elif key == Qt.Key_Up:
+            self.rotate_floating_piece(1)
+
+        self.update()
--- Tetris/tetris_tetromino.py
+++ Tetris/tetris_tetromino.py
@@ -1,71 +1,126 @@
 
-import enum
+from enum import IntEnum
 import random
+
+# PyQt5 imports
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 
 
 # List of orientations
-class Orientation(enum.Enum):
+class Orientation(IntEnum):
     UP = 0
     LEFT = 1
     DOWN = 2
     RIGHT = 3
 
+    def next(self):
+        new_index = self.value + 1
+        if new_index > 3:
+            new_index = 0
+        return new_index
+
+    def prev(self):
+        new_index = self.value - 1
+        if new_index < 0:
+            new_index = 3
+        return new_index
+
 
 # List of colours
-class Shape(enum.Enum):
+class Shape(IntEnum):
     NoShape = 0
     ZShape = 1
     SShape = 2
-    LineShape = 3
+    IShape = 3
     TShape = 4
-    SquareShape = 5
+    OShape = 5
     LShape = 6
-    MirroredLShape = 7
+    JShape = 7
 
 
 class Tetromino():
 
     coordinate_table = (
-        [[0, 0], [0, 0], [0, 0], [0, 0]],
-        [[0, -1], [0, 0], [-1, 0], [-1, 1]],
-        [[0, -1], [0, 0], [1, 0], [1, 1]],
-        [[0, -1], [0, 0], [0, 1], [0, 2]],
-        [[-1, 0], [0, 0], [1, 0], [0, 1]],
-        [[0, 0], [1, 0], [0, 1], [1, 1]],
-        [[-1, -1], [0, -1], [0, 0], [0, 1]],
-        [[1, -1], [0, -1], [0, 0], [0, 1]]
+        # No Shape
+        [[[0, 0],   [0, 0],     [0, 0],     [0, 0]],
+         [[0, 0],   [0, 0],     [0, 0],     [0, 0]],
+         [[0, 0],   [0, 0],     [0, 0],     [0, 0]],
+         [[0, 0],   [0, 0],     [0, 0],     [0, 0]]],
+
+        # Z Shape
+        [[[-1, -1],  [0, -1],    [0, 0],     [1, 0]],
+         [[1, -1],   [1, 0],     [0, 0],     [0, 1]],
+         [[1, 1],    [0, 1],     [0, 0],     [-1, 0]],
+         [[-1, 1],   [-1, 0],    [0, 0],     [0, -1]]],
+
+        # S Shape
+        [[[-1, 0],   [0, 0],     [0, -1],    [1, -1]],
+         [[0, -1],   [0, 0],     [1, 0],     [1, 1]],
+         [[1, 0],    [0, 0],     [0, 1],     [-1, 1]],
+         [[0, 1],    [0, 0],     [-1, 0],    [-1, -1]]],
+
+        # I Shape
+        [[[0, 0],    [-1, 0],    [1, 0],     [2, 0]],
+         [[0, 0],    [0, 0],     [0, 0],     [0, 0]],
+         [[0, 0],    [0, 0],     [0, 0],     [0, 0]],
+         [[0, 0],    [0, 0],     [0, 0],     [0, 0]]],
+
+        # T Shape
+        [[[-1, 0],   [0, 0],     [1, 0],     [0, -1]],
+         [[0, 0],    [0, 0],     [0, 0],     [0, 0]],
+         [[0, 0],    [0, 0],     [0, 0],     [0, 0]],
+         [[0, 0],    [0, 0],     [0, 0],     [0, 0]]],
+
+        # O Shape
+        [[[0, 0],    [1, 0],     [0, -1],    [1, -1]],
+         [[0, 0],    [0, 0],     [0, 0],     [0, 0]],
+         [[0, 0],    [0, 0],     [0, 0],     [0, 0]],
+         [[0, 0],    [0, 0],     [0, 0],     [0, 0]]],
+
+        # L Shape
+        [[[-1, 0],   [0, 0],     [1, 0],     [1, -1]],
+         [[0, 0],    [0, 0],     [0, 0],     [0, 0]],
+         [[0, 0],    [0, 0],     [0, 0],     [0, 0]],
+         [[0, 0],    [0, 0],     [0, 0],     [0, 0]]],
+
+        # J Shape
+        [[[-1, -1],  [-1, 0],    [0, 0],     [1, 0]],
+         [[0, 0],    [0, 0],     [0, 0],     [0, 0]],
+         [[0, 0],    [0, 0],     [0, 0],     [0, 0]],
+         [[0, 0],    [0, 0],     [0, 0],     [0, 0]]]
     )
 
-    def __init__(self, center):
+    def __init__(self, piece_center):
 
-        self.center = center
+        self.center = piece_center
         self.orientation = Orientation.UP
         self.identity = Shape.NoShape
-        self.coordinates = Tetromino.coordinate_table[0]
+        self.coordinates = Tetromino.coordinate_table[0][0]
 
         self.random_assign_shape()
 
     def random_assign_shape(self):
 
-        random_shape = random.randint(1, 7)
-        self.identity = Shape(random_shape)
-        self.coordinates = Tetromino.coordinate_table[random_shape]
-        self.update_center(self.center[0], self.center[1])
+        value = random.randint(1, 7)
+        self.identity = Shape(value)
+        self.coordinates = Tetromino.coordinate_table[value][0]
+        self.move_to_center()
 
     def update_center(self, x, y):
-
         self.center = (x, y)
-        print(self.coordinates)
+
+    def move_to_center(self):
         temp_coordinates = []
-        for positions in self.coordinates:
-            temp_coordinates.append([positions[0] + x, positions[1] + y])
+        for positions in Tetromino.coordinate_table[self.identity.value][self.orientation.value]:
+            temp_coordinates.append([positions[0] + self.center[0], positions[1] + self.center[1]])
         self.coordinates = temp_coordinates
 
     def move_by(self, x, y):
+        self.update_center(self.center[0] + x, self.center[1] + y)
+        self.move_to_center()
 
-        temp_coordinates = []
-        for positions in self.coordinates:
-            new_x = positions[0] + x
-            new_y = positions[1] + y
-            temp_coordinates.append([new_x, new_y])
-        self.coordinates = temp_coordinates
+    def rotate_left(self):
+        self.orientation = Orientation(self.orientation.next())
+
